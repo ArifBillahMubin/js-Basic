13c342d0941235a01f6b96ecb2b2f1c6672a3f35
arif <arifbillahmubin@gmail.com>
2026-08-27 11:41:02 +0600

    object

--- object.js
+++ object.js
@@ -4,6 +4,7 @@ const student = {
     marks: 75
 };
 
+
 const checkStudent = (student) => {
     if(student.marks >= 50){
         console.log(`${student.name} passed.`);
@@ -13,5 +14,11 @@ const checkStudent = (student) => {
 }
 checkStudent(student);
 
+//destructuring
 const {name, age, marks} = student;
 console.log(name, age, marks);
+
+//for in loop used to print all key and value
+for (let key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
