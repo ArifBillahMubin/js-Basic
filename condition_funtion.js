89e05e30a70bac49d907ce8696335123e6fff04b
arif <arifbillahmubin@gmail.com>
2026-08-27 10:27:41 +0600

    datatype loops and condition

---
+++ condition_funtion.js
@@ -0,0 +1,6 @@
+const numbers = [10, 15, 20, 25, 30, 35, 40];
+const result = [];
+
+for(let item of numbers){
+    (20<=item)?result.push('pass'):result.push('fail');
+}
---
+++ dataType.js
@@ -0,0 +1,13 @@
+console.log(a);
+var a = 10;
+
+console.log(a);
+
+//dataType
+let name = "Mehedi";
+let age = 25;
+let isStudent = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
